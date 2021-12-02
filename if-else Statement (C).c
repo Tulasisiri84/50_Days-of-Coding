21a3560d82dbf5b1149b90c4defde04fd1d97aac
Tulasisiri84 <95116938+Tulasisiri84@users.noreply.github.com>
2021-12-02 08:06:44 +0530

    Add files via upload

---
+++ if-else Statement (C).c
@@ -0,0 +1,22 @@
+/*
+Program: Write a C Program if a person is eligible to vote or not
+Date   : 2.11.2021
+Logic  : if-else statement
+*/
+#include <stdio.h>
+int main()
+{
+    int age;
+    printf("Enter your age : ");
+    scanf("%d",&age);
+    if(age>=18)
+    {
+      printf("You are eligible to vote");
+    }
+    else
+    {
+      printf("Your age is %d",age);
+      printf("\nYou are lessthan 18");
+    }
+    return 0;
+}
